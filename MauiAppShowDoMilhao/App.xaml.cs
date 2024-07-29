2eb49112d4bd3e6148aa2d94853cd1e7e9d14684
Usuario <Usuario@CPD03-M20>
2024-07-29 15:11:23 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppShowDoMilhao/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MauiAppShowDoMilhao.Models;
+
+namespace MauiAppShowDoMilhao
+{
+    public partial class App : Application
+    {
+        List<Pergunta> perguntas_faceis = new()
+        {
+            new Pergunta
+            {
+                Id=1,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativas = new()
+            }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppShowDoMilhao/Models/Alternativa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppShowDoMilhao.Models
+{
+    public class Alternativa
+    { 
+        public int Id_Pergunta { get; set; }
+        public string Descricao { get; set; }
+        public bool Correta { get; set; }
+    }
+}
